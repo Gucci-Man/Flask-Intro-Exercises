673ddd315eea0597ff0878b979b99bd81fc64cb1
Adel <adel.ngo@yahoo.com>
2023-08-08 22:12:19 -0500

    Finished greet app

--- greet/app.py
+++ greet/app.py
@@ -6,10 +6,37 @@ app.config["DEBUG"] = True
 
 @app.route("/welcome")
 def welcome():
+    """View that responds with 'Welcome'"""
     html = """
     <html>
         <body>
-            <h1>Welcome</h1>
+            <h1>welcome</h1>
+        </body>
+    </html>
+    """
+    return html
+
+
+@app.route("/welcome/home")
+def welcome_home():
+    """View that responds with 'Welcome bome"""
+    html = """
+    <html>
+        <body>
+            <h1>welcome home</h1>
+        </body>
+    </html>
+    """
+    return html
+
+
+@app.route("/welcome/back")
+def welcome_back():
+    """View that responds with 'Welcome back"""
+    html = """
+    <html>
+        <body>
+            <h1>welcome back</h1>
         </body>
     </html>
     """
